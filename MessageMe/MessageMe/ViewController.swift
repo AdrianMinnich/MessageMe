c4189c460544afef4762aa177f58512c4a27c2ca
Adrian Minnich <adrianminnich@MacBook-Air.local>
2021-05-17 00:30:05 +0200

    initial commit

---
+++ MessageMe/MessageMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MessageMe
+//
+//  Created by Adrian Minnich on 16/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
