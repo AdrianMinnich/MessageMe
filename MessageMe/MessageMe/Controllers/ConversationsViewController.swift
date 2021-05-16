5ca2f4d5033a05a433f5909d790365be2700a5f4
Adrian Minnich <adrianminnich@MacBook-Air.local>
2021-05-17 00:43:43 +0200

    creating needed view controllers

--- MessageMe/MessageMe/Controllers/ConversationsViewController.swift
+++ MessageMe/MessageMe/Controllers/ConversationsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ConversationsViewController.swift
 //  MessageMe
 //
 //  Created by Adrian Minnich on 16/05/2021.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ConversationsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
