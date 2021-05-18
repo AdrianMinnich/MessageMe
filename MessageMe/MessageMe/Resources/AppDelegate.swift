847608e0fe1ac65be1683526070afe11a2bab17a
Adrian Minnich <adrianminnich@MacBook-Air.local>
2021-05-18 11:03:40 +0200

    login and register form, setting up firebase

--- MessageMe/MessageMe/Resources/AppDelegate.swift
+++ MessageMe/MessageMe/Resources/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 @available(iOS 13.0, *)
 @main
@@ -14,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
